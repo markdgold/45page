df23726897b779a84e6a995fb8cc0c76d3db034e
Mark Goldstein <Mark Goldstein>
2017-03-21 08:57:56 -0700

    grade slider more touch friendly

--- js/filter_climbs.js
+++ js/filter_climbs.js
@@ -1,17 +1,3 @@
-//all check box
-// document.addEventListener("DOMContentLoaded", function (event) {
-//     var allSelector = document.querySelector('input[name=all]');
-//     allSelector.addEventListener('change', function (event) {
-//         if (allSelector.checked) {
-//             $("section").find('article').css("display", "inline-block");
-//             console.log("all checked");
-//         } else {
-//         	console.log("all unchecked")
-//         	$("section").find('article').css("display", "none");
-//         }
-//     });
-// });
-
 //pop up filter
 var filterForm = document.getElementById("filterForm");
 var filterLink = document.getElementById("filterLink");
@@ -25,147 +11,24 @@ function filterDisplay(){
         filterForm.style.display = 'inline-block';
         filterLink.style.fontWeight = "bold";
         filterLink.style.background = "white";
-
     }
 }
 
 document.addEventListener("DOMContentLoaded", function (event){
-	filterLink.addEventListener('click',filterDisplay)
+    filterLink.addEventListener('click',filterDisplay)
 });
 
-//filter form
+
+
+//filter form selections
 var powerSelector = document.querySelector('input[name=power]');
 var techSelector = document.querySelector('input[name=tech]');
 var crimpSelector = document.querySelector('input[name=crimp]');
 var oneMoverSelector = document.querySelector('input[name=oneMover]');
 var enduroSelector = document.querySelector('input[name=enduro]');
-var openHandSelector = document.querySelector('input[name=openHand]');
-var allSelector = document.querySelector('input[name=all]');
-
-//Power check box
-function powerBox(event){
-	if (powerSelector.checked) {
-            document.getElementsByName('all')[0].checked = false;
-            allBox(event);
-            $("section").find('.power').css("display", "inline-block");
-        } else {
-        	$("section").find('.power').css("display", "none");
-        }
-}
-
-document.addEventListener("DOMContentLoaded", function (event) {
-    powerSelector.addEventListener('change', powerBox)
-});
-
-//enduro check box
-function enduroBox(event){
-    if (enduroSelector.checked) {
-            document.getElementsByName('all')[0].checked = false;
-            allBox(event);
-            $("section").find('.enduro').css("display", "inline-block");
-        } else {
-            $("section").find('.enduro').css("display", "none");
-        }
-}
-
-document.addEventListener("DOMContentLoaded", function (event) {
-    enduroSelector.addEventListener('change', enduroBox)
-});
-
-//openHand check box
-function openHandBox(event){
-    if (openHandSelector.checked) {
-            document.getElementsByName('all')[0].checked = false;
-            allBox(event);
-            $("section").find('.openHand').css("display", "inline-block");
-        } else {
-            $("section").find('.openHand').css("display", "none");
-        }
-}
-
-document.addEventListener("DOMContentLoaded", function (event) {
-    openHandSelector.addEventListener('change', openHandBox)
-});
-
-//oneMover check box
-function oneMoverBox(event){
-    if (oneMoverSelector.checked) {
-            document.getElementsByName('all')[0].checked = false;
-            allBox(event);
-            $("section").find('.oneMover').css("display", "inline-block");
-        } else {
-            $("section").find('.oneMover').css("display", "none");
-        }
-}
-
-document.addEventListener("DOMContentLoaded", function (event) {
-    oneMoverSelector.addEventListener('change', oneMoverBox)
-});
-
-//tech check box
-function techBox(event){
-	if (techSelector.checked) {
-            document.getElementsByName('all')[0].checked = false;
-            allBox(event);
-            $("section").find('.tech').css("display", "inline-block");
-        } else {
-        	$("section").find('.tech').css("display", "none");
-        }
-}
-
-document.addEventListener("DOMContentLoaded", function (event) {
-    techSelector.addEventListener('change', techBox)
-});
-
-//crimp check box
-function crimpBox(event){
-	if (crimpSelector.checked) {
-            document.getElementsByName('all')[0].checked = false;
-            allBox(event);
-            $("section").find('.crimp').css("display", "inline-block");
-        } else {
-        	$("section").find('.crimp').css("display", "none");
-        }
-}
-
-document.addEventListener("DOMContentLoaded", function (event) {
-    crimpSelector.addEventListener('change', crimpBox)
-});
-
-//All check box
-function allBox(event){
-	if (allSelector.checked) {
-            document.getElementsByName('crimp')[0].checked = false;
-            crimpBox(event);
-            document.getElementsByName('power')[0].checked = false;
-            powerBox(event);
-            document.getElementsByName('tech')[0].checked = false;
-            techBox(event);
-            $("section").find('article').css("display", "inline-block");
-        } else {
-        	$("section").find('article').css("display", "none");
-        }
-}
-
-document.addEventListener("DOMContentLoaded", function (event) {
-    var allSelector = document.querySelector('input[name=all]');
-    allSelector.addEventListener('change', allBox)
-});
-
-//new check box
+var newSelector = document.querySelector('input[name=new]');
 var year = new Date().getFullYear();
 var month = new Date().getMonth()+1;
-document.addEventListener("DOMContentLoaded", function (event) {
-    var _selector = document.querySelector('input[name=new]');
-    _selector.addEventListener('change', function (event) {
-        if (_selector.checked) {
-            $("section").find('article:not(.' +year+')').css("display", "none");
-            $("section").find('article:not(.' +month+')').css("display", "none");
-        } else {
-            $("section").find('article').css("display", "inline-block");
-        }
-    });
-});
 
 //grade selector
 var stepSlider = document.getElementById('slider-step');
@@ -187,19 +50,62 @@ var stepValues = [
 
 stepSlider.noUiSlider.on('update', function( values, handle ) {
   stepValues[handle].innerHTML = "V"+Math.trunc(values[handle]);
-  for(i=0; i<climbs.length; i++){
-    if(climbs[i].grade>=stepSlider.noUiSlider.get()[0] && climbs[i].grade<=stepSlider.noUiSlider.get()[1]){
-        document.getElementById(climbs[i].name).style.display = "inline-block";
-    }
-    else{
-        document.getElementById(climbs[i].name).style.display = "none";
-    }
-  }
+  filterClimbs();
 });
+//
+
+function checkClimb(){
+    for(i=0;i<climbs.length;i++){
+        var climb = $('[data-id ='+i+']');        
+        if(powerSelector.checked && climb.hasClass('power')){
+            climb.css("display", "inline-block");
+        }
+        else if(techSelector.checked && climb.hasClass('tech')){
+            climb.css("display", "inline-block");
+        }
+        else if(crimpSelector.checked && climb.hasClass('crimp')){
+            climb.css("display", "inline-block");
+        }
+        else if(oneMoverSelector.checked && climb.hasClass('oneMover')){
+            climb.css("display", "inline-block");
+        }
+        else if(enduroSelector.checked && climb.hasClass('enduro')){
+            climb.css("display", "inline-block");
+        }
+        else if(powerSelector.checked == false && techSelector.checked == false && crimpSelector.checked == false && oneMoverSelector.checked == false && enduroSelector.checked == false){
+            climb.css("display", "inline-block");
+        }
+        else{
+            climb.css("display", "none");
+        }
+        if(newSelector.checked){
+            $("section").find('article:not(.' +year+')').css("display", "none");
+            $("section").find('article:not(.' +month+')').css("display", "none");
+        }
+        if(climbs[i].grade >stepSlider.noUiSlider.get()[1] || climbs[i].grade <stepSlider.noUiSlider.get()[0]){
+            climb.css("display", "none");
+        }
+    }
+}
+
+function filterClimbs(){
+    $('article').css('display', 'none');
+    checkClimb()
+}
+
+
+powerSelector.addEventListener('change',filterClimbs);
+crimpSelector.addEventListener('change',filterClimbs);
+enduroSelector.addEventListener('change', filterClimbs);
+techSelector.addEventListener('change',filterClimbs);
+oneMoverSelector.addEventListener('change',filterClimbs);
+newSelector.addEventListener('change',filterClimbs);
+
 
 //set by
 function setBy(event){
     var setBy = this.options[this.selectedIndex].text;
+    console.log(document.getElementById('MDGset').innerHTML)
     for(i=0; i<climbs.length; i++){
         if(setBy == 'All'){
             document.getElementById(climbs[i].name).style.display = "inline-block";
--- js/make_article.js
+++ js/make_article.js
@@ -18,9 +18,9 @@ var climbs = [
 		name: 'mysticSmilez',
 		displayGrade: 'V7',
 		grade: 7,
-		styles: ['power','tech'],
+		styles: 'tech',
 		setBy: 'MDG',
-		date: '2/23/16',
+		date: '2/23/17',
 		monthSet: 2,
 		yearSet: 2017,
 		climbImage: 'http://i.imgur.com/KG2y2NS.jpg'
@@ -30,7 +30,7 @@ var climbs = [
 		name: 'sphereGrippin',
 		displayGrade: 'V8',
 		grade: 8,
-		styles: ['power','openHand'],
+		styles: 'power',
 		setBy: 'MDG',
 		date: '5/10/16',
 		monthSet: 5,
@@ -42,10 +42,10 @@ var climbs = [
 		name: 'soManyCircles',
 		displayGrade: 'V8',
 		grade: 8,
-		styles: ['power','openHand'],
+		styles: 'power',
 		setBy: 'MDG',
 		date: '5/10/16',
-		monthSet: 5,
+		monthSet: 3,
 		yearSet: 2016,
 		climbImage: 'http://i.imgur.com/R6ttzpn.jpg'
 	},
@@ -54,7 +54,7 @@ var climbs = [
 		name: 'briansProj',
 		displayGrade: 'V9',
 		grade: 9,
-		styles: ['power'],
+		styles: 'power',
 		setBy: 'MDG',
 		date: '5/10/16',
 		monthSet: 5,
@@ -66,7 +66,7 @@ var climbs = [
 		name: 'redGiant',
 		displayGrade: 'V6',
 		grade: 6,
-		styles: ['crimp','short'],
+		styles: 'crimp',
 		setBy: 'MDG',
 		date: '5/10/16',
 		monthSet: 5,
@@ -86,9 +86,7 @@ function makeClimbs(){
 		climbArticle.className += " " + climbs[i].displayGrade;
 		climbArticle.className += " " + climbs[i].monthSet;
 		climbArticle.className += " " + climbs[i].yearSet;
-		for(j=0; j<climbs[i].styles.length; j++){
-		 	climbArticle.className += " " + climbs[i].styles[j];
-		};
+		climbArticle.className += " " + climbs[i].styles;
 		climbArticle.style.backgroundImage = "url("+climbs[i].climbImage+")";
 		section.appendChild(climbArticle);
 
@@ -113,46 +111,46 @@ function makeClimbs(){
 
 //add new climb to climbs list: ("Name", grade, [styles], "set by", month, day, year, "imgur link")
 
-var bigCrossRight = new Climb("Big Cross Right", 9,['crimp'], "DNA", 12, 13, 2016, "http://i.imgur.com/lxIqrUn.jpg");
+var bigCrossRight = new Climb("Big Cross Right", 9,'crimp', "DNA", 12, 13, 2016, "http://i.imgur.com/lxIqrUn.jpg");
 climbs.push(bigCrossRight);
 
-var pinchCrimp25 = new Climb("Pinch Crimp 25", 6, ['crimp', 'power'], 'MDG', 5, 10, 2016, "http://i.imgur.com/fwuxaqi.jpg");
+var pinchCrimp25 = new Climb("Pinch Crimp 25", 6, 'power', 'MDG', 5, 10, 2016, "http://i.imgur.com/fwuxaqi.jpg");
 climbs.push(pinchCrimp25);
 
-var gonzo = new Climb("Gonzo", 7, ['crimp'], 'MDG', 5, 10, 2016, "http://i.imgur.com/O7ECFVz.jpg");
+var gonzo = new Climb("Gonzo", 7, 'crimp', 'MDG', 5, 10, 2016, "http://i.imgur.com/O7ECFVz.jpg");
 climbs.push(gonzo)
 
-var loafs = new Climb("Loafs", 9, ['openHand', 'power'], 'MDG', 2, 16, 2016, "http://i.imgur.com/ApD0Iqw.jpg")
+var loafs = new Climb("Loafs", 9, 'power', 'MDG', 2, 16, 2017, "http://i.imgur.com/ApD0Iqw.jpg")
 climbs.unshift(loafs);
 
-var stinkyPinky = new Climb("Stinky Pinky", 6, ['power'], 'MDG', 2, 23, 2016, "http://i.imgur.com/gHtDkfg.jpg");
+var stinkyPinky = new Climb("Stinky Pinky", 6, 'power', 'MDG', 2, 23, 2017, "http://i.imgur.com/gHtDkfg.jpg");
 climbs.unshift(stinkyPinky);
 
-var woodMixed = new Climb("Wood Mixed", 8, ['crimp'], 'MDG', 5, 10, 2016, "http://i.imgur.com/gLLYYfy.jpg");
+var woodMixed = new Climb("Wood Mixed", 8, 'crimp', 'MDG', 5, 10, 2016, "http://i.imgur.com/gLLYYfy.jpg");
 climbs.push(woodMixed);
 
-var pinchPinch25 = new Climb("Pinch Pinch 25", 6, ['openHand', 'power'], 'MDG', 5, 10, 2016, "http://i.imgur.com/HEN3o6R.jpg");
+var pinchPinch25 = new Climb("Pinch Pinch 25", 6, 'power', 'MDG', 5, 10, 2016, "http://i.imgur.com/HEN3o6R.jpg");
 climbs.push(pinchPinch25);
 
-var pullPow = new Climb("Pull Pow", 7, ['power'], 'MDG', 5, 10, 2016, "http://i.imgur.com/mPQmTd6.jpg");
+var pullPow = new Climb("Pull Pow", 7, 'power', 'MDG', 5, 10, 2016, "http://i.imgur.com/mPQmTd6.jpg");
 climbs.push(pullPow);
 
-var bigPull25 = new Climb("Big Pull 25", 6, ['power', 'openHand'], 'MDG', 5, 10, 2016, "http://i.imgur.com/HlwVy6W.jpg");
+var bigPull25 = new Climb("Big Pull 25", 6, 'power', 'MDG', 5, 10, 2016, "http://i.imgur.com/HlwVy6W.jpg");
 climbs.push(bigPull25);
 
-var obeyGravity = new Climb("Obey Gravity", 7, ['power', 'openHand'], 'MDG', 5, 10, 2016, "http://i.imgur.com/5enasSN.jpg");
+var obeyGravity = new Climb("Obey Gravity", 7, 'power', 'MDG', 5, 10, 2016, "http://i.imgur.com/5enasSN.jpg");
 climbs.push(obeyGravity);
 
-var dyno = new Climb("Dyno", 6, ['power', 'oneMover'], 'MDG', 5, 10, 2016, "http://i.imgur.com/tstmYED.jpg");
+var dyno = new Climb("Dyno", 6, 'oneMover', 'MDG', 5, 10, 2016, "http://i.imgur.com/tstmYED.jpg");
 climbs.push(dyno);
 
-var theFuture = new Climb("The Future (hold for 3 sec)", 12, ['oneMover'], 'MDG', 5, 10, 2016, "http://i.imgur.com/EXo4DXl.jpg");
+var theFuture = new Climb("The Future (hold for 3 sec)", 12, 'oneMover', 'MDG', 5, 10, 2016, "http://i.imgur.com/EXo4DXl.jpg");
 climbs.push(theFuture);
 
 
 
 
-var pinchPunch = new Climb("Pinch Punch", 9, ['power', 'openHand'], "MDG", 3, 2, 2017, "http://i.imgur.com/ZP4bFn5.jpg");
+var pinchPunch = new Climb("Pinch Punch", 9, 'power', "MDG", 3, 2, 2017, "http://i.imgur.com/ZP4bFn5.jpg");
 climbs.unshift(pinchPunch);
 
 makeClimbs();
